6bd2797201bffbfb59b275b9a21e2ed5f6269dfa
shach <shach@SHWORKSTATION>
2025-05-14 22:17:48 +0500

    Change

--- brok1.Instance/Services/BackgroundServices/PollingBackgroundService.cs
+++ brok1.Instance/Services/BackgroundServices/PollingBackgroundService.cs
@@ -1,25 +1,68 @@
 using brok1.Instance.Services.Data;
+using brok1.Instance.Types.Utils;
+using brok1.Instance.Types;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot;
+using Telegram.Bot.Types;
+using Microsoft.Extensions.Options;
 
 namespace brok1.Instance.Services;
 
 public class PollingBackgroundService(
     ILogger<PollingBackgroundService> logger,
     ITelegramBotClient botClient,
-    UpdateQueueService updateQueueService) : BackgroundService
+    UpdateQueueService updateQueueService,
+    IOptions<BotConfiguration> botConfig) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        await botClient.DeleteWebhook();
+        Settings.LoadAllSettings(botConfig.Value);
+        BotInfo.bot = await botClient.GetMe();
+        BotInfo.botClient = botClient;
+        await InitializeAllGIFsAndPhotos(botClient);
+
         logger.LogInformation("Starting polling background service");
         await EnqueueAllUpdates(stoppingToken);
     }
 
+    private async Task InitializeAllGIFsAndPhotos(ITelegramBotClient botClient)
+    {
+        using var photoStream = File.OpenRead("1.png");
+        using var gif_3Star = File.OpenRead("3star.mp4");
+        using var gif_4Star = File.OpenRead("4star.mp4");
+        using var gif_5Star = File.OpenRead("5star.mp4");
+
+        var photoFile = new InputFileStream(photoStream, "1.png");
+        var gif_3StarFile = new InputFileStream(gif_3Star, "3star.mp4");
+        var gif_4StarFile = new InputFileStream(gif_4Star, "4star.mp4");
+        var gif_5StarFile = new InputFileStream(gif_5Star, "5star.mp4");
+
+        foreach (var gif in BotFile.AllGIFs)
+        {
+            gif.file_id = gif.file_name switch
+            {
+                "5star.mp4" => (await botClient.SendAnimation(BotUser.ADMINS[0], gif_5StarFile)).Animation!.FileId,
+                "4star.mp4" => (await botClient.SendAnimation(BotUser.ADMINS[0], gif_4StarFile)).Animation!.FileId,
+                "3star.mp4" => (await botClient.SendAnimation(BotUser.ADMINS[0], gif_3StarFile)).Animation!.FileId,
+                _ => throw new NotSupportedException()
+            };
+        }
+
+        foreach (var photo in BotFile.AllPhotos)
+        {
+            photo.file_id = photo.file_name switch
+            {
+                "1.png" => (await botClient.SendPhoto(BotUser.ADMINS[0], photoFile)).Photo![0].FileId,
+                _ => throw new NotSupportedException()
+            };
+        }
+
+    }
+
     private async Task EnqueueAllUpdates(CancellationToken stoppingToken)
     {
-        await botClient.DeleteWebhook();
-
         int? offset = null;
         while (!stoppingToken.IsCancellationRequested)
         {
